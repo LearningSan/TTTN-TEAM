b9945d3b226eed1460da40994ea9d3ce26efe96f
ThanhTai <nguyenthanhtai794@gmail.com>
2026-03-31 23:37:40 +0700

    feat(venue): get all venue

---
+++ be-admin/src/main/java/com/example/tttnbe/venue/service/VenueService.java
@@ -0,0 +1,9 @@
+package com.example.tttnbe.venue.service;
+
+import com.example.tttnbe.venue.dto.VenueResponse;
+
+import java.util.List;
+
+public interface VenueService {
+    public List<VenueResponse> getVenues();
+}
