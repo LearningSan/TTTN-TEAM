6054f59f27b3ae35201567807b120a93b246d9be
kphuccc <hokhoiphuc123@gmail.com>
2026-04-06 16:11:08 +0700

    Revert "Update ConcertServiceImpl.java"
    This reverts commit fdcc16ffdb35bb04bc170c6d55b8b821253cffb7.

--- be-admin/src/main/java/com/example/tttnbe/concert/service/ConcertServiceImpl.java
+++ be-admin/src/main/java/com/example/tttnbe/concert/service/ConcertServiceImpl.java
@@ -10,6 +10,7 @@ import com.example.tttnbe.concert.entity.Concert;
 import com.example.tttnbe.concert.repository.ConcertRepository;
 import com.example.tttnbe.common.exception.CustomException;
 import com.example.tttnbe.seat.entity.Seat;
+import com.example.tttnbe.seat.repository.SeatRepository;
 import com.example.tttnbe.venue.entity.Venue;
 import com.example.tttnbe.venue.repository.VenueRepository;
 import com.example.tttnbe.zone.dto.ZoneRequest;
@@ -44,6 +45,9 @@ public class ConcertServiceImpl implements ConcertService {
 
     @Autowired
     private ZoneRepository zoneRepository;
+
+    @Autowired
+    private SeatRepository seatRepository;
 
     //dung chung - bien entity thanh dto
     private ConcertResponse mapToResponse(Concert concert) {
@@ -159,6 +163,10 @@ public class ConcertServiceImpl implements ConcertService {
                             seat.setStatus("AVAILABLE");
                             allSeatsToSave.add(seat);
                         }
+                    }
+
+                    if (!allSeatsToSave.isEmpty()) {
+                        seatRepository.saveAll(allSeatsToSave);
                     }
                 }
             }
